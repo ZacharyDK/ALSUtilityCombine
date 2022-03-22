0bc58d9553f140d69eefcbd15739defbab27a9ac
designsbyd <designsbyd@gmail.com>
2022-03-21 22:01:36 -0700

    Update ALSCharacterMovementComponent.cpp
    Minor adjustments to make compiling for UE5 Preview 2 possible.

--- Plugins/ALS-Community-main/Source/ALSV4_CPP/Private/Character/ALSCharacterMovementComponent.cpp
+++ Plugins/ALS-Community-main/Source/ALSV4_CPP/Private/Character/ALSCharacterMovementComponent.cpp
@@ -168,15 +168,15 @@ float UALSCharacterMovementComponent::GetMappedSpeed() const
 
 	if (Speed > LocRunSpeed)
 	{
-		return FMath::GetMappedRangeValueClamped({LocRunSpeed, LocSprintSpeed}, {2.0f, 3.0f}, Speed);
+		return FMath::GetMappedRangeValueClamped(FVector2D{LocRunSpeed, LocSprintSpeed}, FVector2D{2.0f, 3.0f}, Speed);
 	}
 
 	if (Speed > LocWalkSpeed)
 	{
-		return FMath::GetMappedRangeValueClamped({LocWalkSpeed, LocRunSpeed}, {1.0f, 2.0f}, Speed);
+		return FMath::GetMappedRangeValueClamped(FVector2D{LocWalkSpeed, LocRunSpeed}, FVector2D{1.0f, 2.0f}, Speed);
 	}
 
-	return FMath::GetMappedRangeValueClamped({0.0f, LocWalkSpeed}, {0.0f, 1.0f}, Speed);
+	return FMath::GetMappedRangeValueClamped(FVector2D{0.0f, LocWalkSpeed}, FVector2D{0.0f, 1.0f}, Speed);
 }
 
 void UALSCharacterMovementComponent::SetMovementSettings(FALSMovementSettings NewMovementSettings)
